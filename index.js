f57fc407a0082d858ff5f14e1182506bee7ab4c0
Mohamed-Kacem-Dev <83512547+Mohamed-Kacem-Dev@users.noreply.github.com>
2024-01-31 19:04:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const io = socketIO(server, {
 
 setupSocket(io);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
